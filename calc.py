a2509a29463a08c9437e194c1f3d67862729273e
Eshatva Mithal <emithal@moore22.cs.purdue.edu>
2015-10-08 12:47:10 -0400

    Added an exponentiation method.

--- calc.py
+++ calc.py
@@ -33,6 +33,13 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Exponent function
+# a -- base
+# b -- power
+def exp(a, b):
+    return a ** b
+
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +74,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+		elif(op == "**"):
+			print "Exponentiation: ", exp(a, b)
         else:
             print "Invalid operation..."
 
